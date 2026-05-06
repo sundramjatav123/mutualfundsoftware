7942b1c9b7eb357209bc8df980fefc00abca16b0
Sundram123 <Sundram.jatav@redvisionglobal.com>
2026-05-06 19:15:08 +0530

    changes

--- src/app/components/sections/FAQSection.jsx
+++ src/app/components/sections/FAQSection.jsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 
 
-export default function FAQSection({faqs}) {
+export default function FAQSection({ faqs }) {
   const [active, setActive] = useState(0);
 
   return (
@@ -34,45 +34,48 @@ export default function FAQSection({faqs}) {
 
         <div className="w-full">
           <div className="flex flex-col gap-4">
-            {faqs.reverse().map((faq, index) => (
-              <motion.div
-                key={index}
-                initial={{ opacity: 0, y: 40 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ delay: index * 0.1 }}
-                className={`border rounded-xl p-4 transition-all duration-300 
-                ${active === index
-                    ? "bg-[var(--rv-card)] shadow-md border-[var(--rv-primary)]"
-                    : "border-[var(--rv-border)] hover:shadow-sm"
-                  }`}
-              >
-                <div
-                  onClick={() => setActive(active === index ? null : index)}
-                  className="flex justify-between items-center cursor-pointer"
-                >
-                  <h6 className="font-semibold">
-                    {index + 1}. {faq?.question}
-                  </h6>
-                  <motion.div
-                    animate={{ rotate: active === index ? 90 : 0 }}
-                    transition={{ duration: 0.3 }}
-                  >
-                    <FiArrowUpRight size={30} />
-                  </motion.div>
-                </div>
+            {faqs
+              .slice()
+              .reverse()
+              .map((faq, index) => (
                 <motion.div
-                  initial={false}
-                  animate={{
-                    height: active === index ? "auto" : 0,
-                    opacity: active === index ? 1 : 0,
-                  }}
-                  transition={{ duration: 0.3 }}
-                  className="overflow-hidden">
-                  <p className="opacity-80 text-lg pt-3">{faq?.answer}</p>
-                </motion.div>
+                  key={index}
+                  initial={{ opacity: 0, y: 40 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{ delay: index * 0.1 }}
+                  className={`border rounded-xl p-4 transition-all duration-300 
+                ${active === index
+                      ? "bg-[var(--rv-card)] shadow-md border-[var(--rv-primary)]"
+                      : "border-[var(--rv-border)] hover:shadow-sm"
+                    }`}
+                >
+                  <div
+                    onClick={() => setActive(active === index ? null : index)}
+                    className="flex justify-between items-center cursor-pointer"
+                  >
+                    <h6 className="font-semibold">
+                      {index + 1}. {faq?.question}
+                    </h6>
+                    <motion.div
+                      animate={{ rotate: active === index ? 90 : 0 }}
+                      transition={{ duration: 0.3 }}
+                    >
+                      <FiArrowUpRight size={30} />
+                    </motion.div>
+                  </div>
+                  <motion.div
+                    initial={false}
+                    animate={{
+                      height: active === index ? "auto" : 0,
+                      opacity: active === index ? 1 : 0,
+                    }}
+                    transition={{ duration: 0.3 }}
+                    className="overflow-hidden">
+                    <p className="opacity-80 text-lg pt-3">{faq?.answer}</p>
+                  </motion.div>
 
-              </motion.div>
-            ))}
+                </motion.div>
+              ))}
 
           </div>
 
